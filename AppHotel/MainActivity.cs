b6fcbc4901e8a8d2009ec87c39b0df10184dbb35
Lucas Metzker <luccazmetzker@gmail.com>
2023-05-19 12:12:20 -0400

    feat: incluindo projeto no github

---
+++ AppHotel/MainActivity.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Snackbar;
+using Android.Widget;
+using Android.Graphics.Drawables;
+
+namespace AppHotel
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        ImageView imgLogo, imgUser, imgPassword;
+        EditText edtUser, edtPassword;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            // Relacionar os elementos da ACT
+            imgLogo     = FindViewById<ImageView>(Resource.Id.imgLogo);
+            imgUser     = FindViewById<ImageView>(Resource.Id.imgUser);
+            imgPassword = FindViewById<ImageView>(Resource.Id.imgPassword);
+            edtUser     = FindViewById<EditText> (Resource.Id.edtUser);
+            edtPassword = FindViewById<EditText> (Resource.Id.edtPassword);
+
+            imgLogo.SetImageResource(Resource.Drawable.logo);
+            imgUser.SetImageResource(Resource.Drawable.usuarios);
+            imgPassword.SetImageResource(Resource.Drawable.senha);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Não tem nada aqui! :)", Snackbar.LengthLong)
+                .SetAction("Action", (View.IOnClickListener)null).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+	}
+}
