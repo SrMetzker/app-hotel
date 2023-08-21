eb8cece51d20e3a1d55a28c1f3af7286b6aa327c
Lucas Metzker <luccazmetzker@gmail.com>
2023-08-21 17:10:23 -0400

    featr: inlcuindo metodos e layouts referentes a movimentacao e gastos
    feat: Foram incluídos os métodos e layouts responsáveis pelas rotinas de movimentação e gerenciamento de gastos

--- AppHotel/Authentication.cs
+++ AppHotel/Authentication.cs
@@ -1,20 +1,19 @@
-﻿using Android.App;
-using Android.Widget;
-using System;
+﻿using AppHotel;
+using Newtonsoft.Json;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 
-public class LoginService
+public class Authentication
 {
     private readonly HttpClient _httpClient;
 
-    public LoginService()
+    public Authentication()
     {
         _httpClient = new HttpClient();
     }
 
-    public async Task<HttpResponseMessage> AuthenticateUser(string usuario, string senha)
+    public async Task<LoginResponse> AuthenticateUser(string usuario, string senha)
     {
         // Constrói a URL da rota de autenticação da API
         string apiUrl = "http://192.168.1.6:8000/login";
@@ -23,7 +22,25 @@ public class LoginService
         var loginData = new { usuario, senha };
 
         // Envia a requisição POST para a rota de autenticação
-        return await _httpClient.PostAsJsonAsync(apiUrl, loginData);
+        HttpResponseMessage httpResponse = await _httpClient.PostAsJsonAsync(apiUrl, loginData);
 
+        LoginResponse response = await DeserializeJsonContentAsync<LoginResponse>(httpResponse);
+
+        return response;
+
+    }
+
+    /// <summary>
+    /// Método responsável por desserializar a resposta da requisição.
+    /// </summary>
+    /// <param name="response">Objeto que será desserializado</param>
+    /// <returns>Retorna um objeto Json desserializado</returns>
+    public async Task<LoginResponse> DeserializeJsonContentAsync<LoginResponse>(HttpResponseMessage response)
+    {
+        string jsonContent = await response.Content.ReadAsStringAsync();
+
+        LoginResponse result = JsonConvert.DeserializeObject<LoginResponse>(jsonContent);
+
+        return result;
     }
 }
--- AppHotel/LoginResponse.cs
+++ AppHotel/LoginResponse.cs
@@ -11,11 +11,13 @@ using System.Text;
 
 namespace AppHotel
 {
-    internal class LoginResponse
+    public class LoginResponse
     {
         public string Authorized { get; set; }
         public string Erro { get; set; }
+        public string Nome { get; set; }
         public string Usuario { get; set; }
+        public string Senha { get; set; }
         public string Cargo { get; set; }
     }
 }
